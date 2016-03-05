cced8e394074b6f9bdbbbeadff54461b655a5dcc
Myles Braithwaite <me@mylesbraithwaite.com>
2016-03-05 17:48:46 -0500

    Fixed up some minor issues.

--- fabfile.py
+++ fabfile.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python
+"""
+Fabric file to working with Myles' Life WordPress setup.
+"""
 
 import datetime
 
 from fabric.utils import error
 from fabric.api import env, task, run, get, roles
 
-env.user = 'myles_myles-life'
-env.hosts = ['ssh.phx.nearlyfreespeech.net']
+env.user = 'myles'
+env.hosts = ['bear']
 env.use_ssh_config = True
 
 env.output_prefix = []
 
 env.roledefs = {
-    'wp': ['ssh.phx.nearlyfreespeech.net']
+    'wp': ['bear']
 }
 
 
 def wp_cli(args):
-    run('wp {0}'.format(args))
+    """Run the wpcli command on the server."""
+    run('wp --path=/srv/www/myles.life/www/html/ {0}'.format(args))
 
 
 @task
 @roles('wp')
 def plugin(arg=None):
-    """
-    Manage the plugins.
-    """
+    """Manage the plugins."""
     if arg == 'update':
         wp_cli('plugin update --all')
     elif arg == 'status':
@@ -39,6 +41,7 @@ def plugin(arg=None):
 @task
 @roles('wp')
 def theme(arg=None):
+    """Manage the themes."""
     if arg == 'update':
         wp_cli('theme update --all')
     elif arg == 'status':
@@ -52,6 +55,7 @@ def theme(arg=None):
 @task
 @roles('wp')
 def db(arg=None):
+    """Manage the database."""
     if arg == 'optimize':
         wp_cli('db optimize')
     elif arg == 'repair':
@@ -77,6 +81,7 @@ def db(arg=None):
 @task
 @roles('wp')
 def core(arg=None):
+    """Manage the WordPress install."""
     if arg == 'update':
         wp_cli('core update')
     elif arg == 'version':
@@ -88,6 +93,7 @@ def core(arg=None):
 @task
 @roles('wp')
 def update_all():
+    """Update everything."""
     plugin('update')
     theme('update')
     core('update')
